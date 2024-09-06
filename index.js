df7a9d2f666b24ca8d0c4a2e48da2e0093408abf
Siphokuhle <siphokuhlenyana@gmail.com>
2024-09-06 09:53:38 +0200

    :construction:

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ app.use((req, res, next) => {
 })
 app.use(express.json())
 app.use(express.static('public'))
-app.use(cors())
+app.use(cors({
+    origin:"*",
+    credentials: true
+}))
 
 app.use('/users',userRouter)
 app.use('/chats',chatsRouter)
