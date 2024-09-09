c5db78b468358c51da5511d45ba1c0d53d4ae0be
Siphokuhle <siphokuhlenyana@gmail.com>
2024-09-09 09:17:48 +0200

    cors problem yho

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ import cors from 'cors'
 
 const app =express()
 let port=process.env.PORT || 5009
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next()
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "*");
+//     res.header("Access-Control-Request-Methods", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Expose-Headers", "Authorization");
+//     next()
+// })
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cors({
