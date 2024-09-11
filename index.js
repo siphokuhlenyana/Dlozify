bc1d72422d9c8396737dbd411c132ebaf02a2e2d
siphokuhlenyana <siphokuhlenyana@gmail.com>
2024-09-11 12:47:05 -0700

    functions

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import userRouter from './router/userRouter.js'
+import chatsRouter from './router/chatsRouter.js'
+import contactRouter from './router/contactRouter.js'
+import msgRouter from './router/msgRouter.js'
+import notificationRouter from './router/notificationRouter.js'
+import requestRouter from './router/requestRouter.js'
+import postRouter from './router/postRouter.js'
+import commentRouter from './router/commentRouter.js'
+
+import cors from 'cors'
+import { verifyAToken } from './middleware/authenticate.js'
+
+const app =express()
+let port=process.env.PORT || 5009
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "*");
+//     res.header("Access-Control-Request-Methods", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Expose-Headers", "Authorization");
+//     next()
+// })
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cors({
+    origin: '*',
+    credentials: true
+})
+)
+
+app.use('/users',userRouter)
+app.use('/chats',chatsRouter)
+app.use('/contacts',contactRouter)
+app.use('/msg',msgRouter)
+app.use('/notifications',notificationRouter)
+app.use('/request',requestRouter)
+app.use('/posts',postRouter)
+app.use('/comment',verifyAToken, commentRouter)
+
+app.listen(port,()=>{
+    console.log("http://localhost:" +port);
+})
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -81,7 +81,7 @@
      
       <div v-else>
         <h2 class="display-5" :style="{color:'#fff'}">Post Management</h2>
-        <button :style="{marginLeft:'948px'}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <button @click="deleteSelected" :style="{marginLeft:'948px'}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
 </svg></button>
@@ -244,7 +244,15 @@ import SpinnerView from './SpinnerView.vue';
       } else {
         this.cookieExists = false;
       }
+    },
+  deleteSelected(post, selected) {
+  post.forEach((item, index) => {
+    if (selected.includes(item)) {
+      post.splice(index, 1);
     }
+  });
+  return post;
+}
         
         }
         ,
@@ -255,7 +263,7 @@ import SpinnerView from './SpinnerView.vue';
     this.checkCookie()
     
   }
-    };
+    }; 
     </script>
     
     <style scoped>
