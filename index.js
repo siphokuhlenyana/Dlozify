bebfc670c45fde6ec8d0032d772928d2cc8ccb1f
Siphokuhle <siphokuhlenyana@gmail.com>
2024-09-12 14:22:04 +0200

    SEND HELPPPPP!

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ app.use((req, res, next) => {
 })
 app.use(express.json())
 app.use(express.static('public'))
-app.use(cors({
-    origin: '*',
-    credentials: true
-})
+app.use(cors()
 )
 
 app.use('/users',userRouter)
