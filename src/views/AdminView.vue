a0e5fa37c5cc33132639c6d15e2aba463dbdac3f
Siphokuhle <siphokuhlenyana@gmail.com>
2024-09-13 14:54:01 +0200

    acc

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -85,7 +85,7 @@
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
 </svg></button>
-        <div v-if="!cookieExists"> <spinner-view/>   </div>
+        <div v-if="cookieExists"> <spinner-view/>   </div>
         
         <table v-else  class="table-primary table-bordered border-primary" id="table" :style="{width:'1000px',height:'100px',borderColor:'#A51196',borderWidth:'1px',borderStyle:'solid',backgroundColor:'A51196'}">
         <thead :style="{fontWeight:'1000',marginBottom:'1000px'}">
